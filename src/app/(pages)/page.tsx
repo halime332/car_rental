6589da92d390d94a87ed90c262f39272ba2fc6f7
halime332 <halimeyavuz1001@gmail.com>
2025-07-13 22:44:30 +0300

    rent car

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -2,7 +2,7 @@
 
 const Page = () => {
   return (
-    <div className="text-black">
+    <div >
       Page
     </div>
   )
--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,13 +1,28 @@
 import Link from "next/link";
+import { CiSearch } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
 
 
 const Header = () => {
   return (
-    <header className="text-black bg-white flex justify-between
+    <header className="bg-white flex justify-between
      items-center py-5 px-7 lg:py-8 lg:px-10">
     
       <Link href="/" className="text-basic-blue font-bold 
       text-2xl lg:text-4xl">DRIVE</Link>
+
+      <form className="flex gap-2 py-2 px-4 rounded-full border border-zinc-300 md:w-1/2 focus:bg-red-500">
+      <button className="text-zinc-700 text-xl">
+       <CiSearch />
+      </button>
+      <input type="text" placeholder="Bir marka aratın" className="outline-none text-zinc-800"/>
+      </form>
+
+      <div>
+        <div>
+          <FaHeart className=""/>
+        </div>
+      </div>
     </header>
   )
 }
