8ea0e3e0e13aca0865b98fe570154b94a30005cd
halime332 <halimeyavuz1001@gmail.com>
2025-07-14 22:54:36 +0300

    car rent

--- src/app/components/hero/index.tsx
+++ src/app/components/hero/index.tsx
@@ -3,10 +3,33 @@
 const Hero = () => {
   return (
     <div className="lg:grid lg.grid-cols-2  lg:gap-5">
-      <div className="p-6 rounded-lg">
-        <h1>Araç Kiralama için <br/>En İyi Platform
+      <div className="p-6 rounded-lg bg-[url('/bg-1.png')] bg-center bg-cover">
+        <h1 className="text-black font-semibold">Araç Kiralama için <br/>En İyi Platform
         </h1>
+        <p className="my-3 text-black">Güvenli ve güvenilir bir şekilde araç kiralama yapmanın kolay.
+          Tabii ki dşük bir fiyata
+        </p>
+
+        <button className="bg-basic-blue py-2 px-3 rounded-md hover:brightness-90 transition text-white">Araç Kirala</button>
+
+        <div className="flex justify-center">
+          <img src="/car-1.png" alt="car" />
+        </div>
       </div>
+      <div className="p-6 rounded-lg">
+        <h1 className="text-black font-semibold">Düşük fiyatla<br/>Araba kiralama
+        </h1>
+        <p className="my-3 text-black">Ucuz araç kiralama hizmetleri le güvenli ve konforlu olanaklar sunmak
+        
+        </p>
+
+        <button className="bg-[#54A6FF] py-2 px-3 rounded-md hover:brightness-90 transition text-white">Araç Kirala</button>
+
+        <div className="flex justify-center">
+          <img src="/car-2.png" alt="car" />
+        </div>
+       </div>
+
       <div></div>
     </div>
   )
